c1f8fa7eea12e284c8076ae3d60492d0a2026091
Henry Grantham <hgrantham@gmail.com>
2015-02-28 18:07:00 -0800

    wrote fixtures for 4 different test requests

--- test_echo_server.py
+++ test_echo_server.py
@@ -17,13 +17,24 @@ def test_server_empty():
 
 def test_server_unicode():
     """Test that a unicode string works"""
-    text = u"this is a test: \u00bd\u0553\u04e7"
+    text = u"this is a test for a unicode string: \u00bd\u0553\u04e7"
+    text = text.encode('utf-8')
     assert echo_client(text) == text
 
 
 def test_server_long_string():
     """Test that a string longer than 32 works"""
     text = u"this is a test of whether a string longer than 32 workds"
+    assert echo_client(text) == text
+
+
+def test_server_buffer_size_string():
+    """Test that a string longer than 32 works"""
+    list1 = []
+    for i in range(0, BUFFERSIZE):
+        list1.append('a')
+    text = ''.join(list1)
+    assert len(text) == BUFFERSIZE
     assert echo_client(text) == text
 
 
@@ -40,24 +51,50 @@ def test_response_error():
     assert 'Not Found' in response_error(404, "Not Found")
 
 
-def test_server_buffer_size_string():
-    """Test that a string longer than 32 works"""
-    list1 = []
-    for i in range(0, BUFFERSIZE):
-        list1.append('a')
-    text = ''.join(list1)
-    assert len(text) == BUFFERSIZE
-    assert echo_client(text) == text
-
-
 def test_parse():
     """tests parse"""
+    # GET request right protocal
+    get_right =
+    assert parse(GET_request_right_protocal()) == get_right
+    # GET request wrong protocal
+    # POST request right protocal
+    # POST request wrong protocal
 
-
-    t = parse("Test")
+    text = parse("Test")
     assert True
 
 
-@fixture
-def good_response():
-    
+@pytest.fixture(scope='function')
+def GET_request_right_protocal():
+    lines = [
+        "GET /index.html HTTP/1.1",
+        "Host: www.test.com",
+    ]
+    return "".join(["\r\n".join(lines), "\r\n"])
+
+
+@pytest.fixture(scope='function')
+def GET_request_wrong_protocal():
+    lines = [
+        "GET /index.html IMAPS",
+        "Host: www.test.com",
+    ]
+    return "".join(["\r\n".join(lines), "\r\n"])
+
+
+@pytest.fixture(scope='function')
+def POST_request_right_protocal():
+    lines = [
+        "POST /index.html HTTP/1.1",
+        "Host: www.test.com",
+    ]
+    return "".join(["\r\n".join(lines), "\r\n"])
+
+
+@pytest.fixture(scope='function')
+def POST_request_wrong_protocal():
+    lines = [
+        "POST /index.html IMAPS",
+        "Host: www.test.com",
+    ]
+    return "".join(["\r\n".join(lines), "\r\n"])
